491696c9097a50601c8fedc183da7b545e8d9312
jackytallow <3434481891@qq.com>
2018-12-28 22:57:44 +0800

    编写相关布局xml

--- StudentManager_System/app/src/main/java/com/example/studentmanager_system/MainActivity.java
+++ StudentManager_System/app/src/main/java/com/example/studentmanager_system/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.admin_login_layout);
+        setContentView(R.layout.activity_main);
     }
 }
--- StudentManager_system/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
+++ StudentManager_system/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
@@ -73,20 +73,20 @@ public final class R {
     public static final class id {
         private id() {}
 
-        public static final int bottom = 0x7f080024;
-        public static final int end = 0x7f080040;
-        public static final int gone = 0x7f08004d;
-        public static final int invisible = 0x7f080057;
-        public static final int left = 0x7f08005c;
-        public static final int packed = 0x7f080070;
-        public static final int parent = 0x7f080072;
-        public static final int percent = 0x7f080075;
-        public static final int right = 0x7f08007a;
-        public static final int spread = 0x7f08009d;
-        public static final int spread_inside = 0x7f08009e;
-        public static final int start = 0x7f0800a3;
-        public static final int top = 0x7f0800b8;
-        public static final int wrap = 0x7f0800c8;
+        public static final int bottom = 0x7f080030;
+        public static final int end = 0x7f08004c;
+        public static final int gone = 0x7f080059;
+        public static final int invisible = 0x7f080063;
+        public static final int left = 0x7f080068;
+        public static final int packed = 0x7f080081;
+        public static final int parent = 0x7f080083;
+        public static final int percent = 0x7f080086;
+        public static final int right = 0x7f08008c;
+        public static final int spread = 0x7f0800af;
+        public static final int spread_inside = 0x7f0800b0;
+        public static final int start = 0x7f0800b5;
+        public static final int top = 0x7f0800d0;
+        public static final int wrap = 0x7f0800e3;
     }
     public static final class styleable {
         private styleable() {}
